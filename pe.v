88b6173273b682496d4193f32d3fc8fa3cbf60f7
varunan0801 <varunan0801@gmail.com>
2026-04-03 01:28:54 +0530

    Update pe.v

--- pe.v
+++ pe.v
@@ -1,42 +1,29 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/28/2025 11:36:07 PM
-// Design Name: 
-// Module Name: pe
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module pe #(
-  parameter DATA_WIDTH = 16
+  parameter integer DATA_WIDTH = 16, //set by the top file
+  parameter integer ACC_WIDTH = 40  //set by the top file
 )(
-  input  wire clk,
-  input  wire reset,
+  input wire clk,
+  input wire reset,
+  input wire clear,
 
-  input  wire signed [DATA_WIDTH-1:0] a_in,
-  input  wire signed [DATA_WIDTH-1:0] b_in,
+  input wire signed [DATA_WIDTH-1:0] a_in,
+  input wire signed [DATA_WIDTH-1:0] b_in,
 
   output reg  signed [DATA_WIDTH-1:0] a_out,
   output reg  signed [DATA_WIDTH-1:0] b_out,
 
-  output reg  signed [2*DATA_WIDTH-1:0] acc
+  output reg  signed [ACC_WIDTH-1:0] acc
 );
 
-  always @(posedge clk) begin
-    if (reset) begin
+//sign extension of a_in and b_in so negative numbers dont get extended with zeroes and turning into a positive number
+wire signed [ACC_WIDTH-1:0] a_ext = {{(ACC_WIDTH - DATA_WIDTH){a_in[DATA_WIDTH-1]}}, a_in};
+wire signed [ACC_WIDTH-1:0] b_ext = {{(ACC_WIDTH - DATA_WIDTH){b_in[DATA_WIDTH-1]}}, b_in};
+
+always @(posedge clk) begin
+    if (reset || clear) begin
       acc   <= 0;
       a_out <= 0;
       b_out <= 0;
@@ -45,6 +32,6 @@ module pe #(
       a_out <= a_in;
       b_out <= b_in;
     end
-  end
+end
 
 endmodule
